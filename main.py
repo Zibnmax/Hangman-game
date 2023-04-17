aa1e9ae02234769fa9b538c58964a91342ec987d
Zibnmax <zibnmax@gmail.com>
2023-04-18 01:17:34 +0300

    make it a class

--- main.py
+++ main.py
@@ -4,73 +4,104 @@ from time import sleep
 
 from ascii_art import hangman_logo, hang, fireworks
 
-def intro() -> None:
-    '''
-    Running inrto and rules
-    '''
-    hangman_logo()
-    print('Чтобы выиграть, нужно отгадать загаданное слово. Вы можете вводить по одной букве за раз.\n\
-Слово - это существительное в единственном числе. У Вас есть 6 попыток. Удачи :)\n')
-    
-    
-def pick_word() -> str:
-    '''
-    Picking a random word from "words.txt"
-    '''
-    try:
-        with open('words.txt', encoding='utf_8') as file:
-            words = file.readlines()
-            return choice(words).rstrip('\n').lower()
-    except:
-        print('Что-то не так со словарем :(')
-        sys.exit()
 
-def main() -> None:
-    '''
-    Run a game
-    '''
-    word = pick_word()
-    result = {i:'_ ' for i in range(0, len(word))}
-    mistakes_letters = []
+class HangmanGame:
+    
+    def __init__(self) -> None:
+        self.word = self.pick_word()
+        self.mistakes_letters = []
+        self.display = {i:'_ ' for i in range(0, len(self.word))}
 
-    while len(mistakes_letters) < 6:
-        
-        hang(mistakes=len(mistakes_letters))
-        print(f'Количество ошибок: {len(mistakes_letters)}')
-        print(f'Введенные Вами буквы, которых в слове нет: {" ".join(mistakes_letters)}')
-        print(f'Слово:    {" ".join(result.values())}')
-        
-        letter = input('\nВведите букву: ')[0].lower()
-        
-        print('\033c')
-        
-        if letter in mistakes_letters:
-            print('Вы уже вводили эту букву, попробуйте другую\n')
-            continue
-        if letter not in word:
-            mistakes_letters.append(letter)
-            print('Такой буквы в слове нет\n')
-            continue
-        
-        print(f'Есть буква "{letter}" в слове.')
-        for i, l in enumerate(word):
-            if l == letter:
-                result.update({i:l})
-        
-        if '_ ' not in result.values():
-            print('\033c')
-            print(f'Вы угадали слово "{word}"! Поздравляю!\n\n')
-            fireworks()
-            sleep(5)
+
+    def run_intro(self) -> None:
+        '''
+        Running inrto and rules
+        '''
+        hangman_logo()
+        self.player_name = input('Чтобы выиграть, нужно отгадать загаданное слово. Вы можете вводить по одной букве за раз.\n\
+Слово - это существительное в единственном числе. У Вас есть 6 попыток. Удачи :)\nЧтобы продолжить, введите своё имя:\n')
+
+
+    def pick_word(self) -> str:
+        '''
+        Picking a random word from "words.txt"
+        '''
+        try:
+            with open('words.txt', encoding='utf_8') as file:
+                words = file.readlines()
+                return choice(words).rstrip('\n').lower()
+        except:
+            print('Что-то не так со словарем :(')
             sys.exit()
 
-    print(f'Вы проиграли!\nЗагаданное слово было\n{word.upper()}')
-    hang(mistakes=len(mistakes_letters))
-    sleep(5)
+    def is_letter_in_word(self, letter) -> bool:
+        '''
+        Checking user input
+        '''
+        if letter not in 'абвгдеёжзийклмнопрстуфхцчшщьыъэюя':
+            print('Пожалуйста, вводите буквы русского алфавита.')
+            return False
+        
+        if letter in self.mistakes_letters:
+            print('Вы уже вводили эту букву, попробуйте другую\n')
+            return False
+
+        if letter not in self.word:
+            self.mistakes_letters.append(letter)
+            print('Такой буквы в слове нет\n')
+            return False
+        
+        return True
+
+
+    def victory(self) -> None:
+        '''
+        Display victory fireworks
+        '''
+        print('\033c')
+        print(f'Вы угадали слово "{self.word.upper()}"! Поздравляю!\n\n')
+        fireworks()
+        sleep(5)
+        sys.exit()
+
+
+    def run_game(self) -> None:
+        '''
+        Run a game
+        '''
+        self.run_intro()
+        print('\033c')
+        
+        while len(self.mistakes_letters) < 6:
+            
+            hang(mistakes=len(self.mistakes_letters))
+            print(f'Количество ошибок: {len(self.mistakes_letters)}')
+            print(f'Введенные Вами буквы, которых в слове нет: {" ".join(self.mistakes_letters)}')
+            print(f'Слово:    {" ".join(self.display.values())}')
+            
+            self.letter = input('\nВведите букву: ')[0].lower()
+            
+            print('\033c')
+            
+            if not self.is_letter_in_word(self.letter):
+                continue
+
+            
+            print(f'Есть буква "{self.letter}" в слове.')
+            for i, l in enumerate(self.word):
+                if l == self.letter:
+                    self.display.update({i:l})
+            
+            if '_ ' not in self.display.values():
+                self.victory()
+
+        print(f'Вы проиграли!\nЗагаданное слово было\n{self.word.upper()}')
+        hang(mistakes=len(self.mistakes_letters))
+        sleep(5)
 
 
 
 if __name__ == '__main__':
     
-    intro()
-    main()
+    game = HangmanGame()
+    game.run_game()
